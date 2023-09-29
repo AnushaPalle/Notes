1c722105e5a768b7e1cf45b5f8c932f18c6f304d
anushapalle <anushapalle11@gmail.com>
2023-09-29 11:44:59 +0530

    Add Variables and functions

--- Trevor-Sawler-Udemy-Modern-WebApps-Go/main.go
+++ Trevor-Sawler-Udemy-Modern-WebApps-Go/main.go
@@ -2,6 +2,28 @@ package main
 
 import "fmt"
 
+var packageLevelVar string = "hi"
+
 func main() {
 	fmt.Println("Hello Ganesh !!")
+
+	// variables
+	var whatToSay string
+	var i int
+
+	whatToSay = "Goodbye, world"
+	fmt.Println(whatToSay)
+
+	i = 7
+
+	fmt.Println("i is set to", i)
+
+	whatWasSaid, theOtherThingThatWasSaid := saySomething() // := assignes return value type of saysomething function to whatWasSaid, theOtherThingThatWasSaid variable type 
+
+	fmt.Println("The function returned", whatWasSaid, theOtherThingThatWasSaid, "packageLevel Variable:", packageLevelVar)
+}
+
+// custom functions 
+func saySomething() (string, string) {
+	return "something", "else"
 }
