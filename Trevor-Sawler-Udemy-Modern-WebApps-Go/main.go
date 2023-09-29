8d90afc91249a1c86a6bad81294033cca0c5a3a2
anushapalle <anushapalle11@gmail.com>
2023-09-29 16:08:31 +0530

    Add Pointers

--- Trevor-Sawler-Udemy-Modern-WebApps-Go/main.go
+++ Trevor-Sawler-Udemy-Modern-WebApps-Go/main.go
@@ -1,6 +1,7 @@
 package main
 
 import "fmt"
+import "log"
 
 var packageLevelVar string = "hi"
 
@@ -21,9 +22,22 @@ func main() {
 	whatWasSaid, theOtherThingThatWasSaid := saySomething() // := assignes return value type of saysomething function to whatWasSaid, theOtherThingThatWasSaid variable type 
 
 	fmt.Println("The function returned", whatWasSaid, theOtherThingThatWasSaid, "packageLevel Variable:", packageLevelVar)
+
+	// Pointers:
+	var myString string = "Green"
+
+	log.Println("myString is set to", myString)
+	changeUsingPointer(&myString)
+	log.Println("after func call myString is set to", myString)
 }
 
 // custom functions 
 func saySomething() (string, string) {
 	return "something", "else"
+}
+
+func changeUsingPointer(s *string) {
+	log.Println("s is set to", s)
+	newValue := "Red"
+	*s = newValue
 }
