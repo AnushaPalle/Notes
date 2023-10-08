83fb9dcded2a5bbb0e3a4688e9b7b58c33138cd9
anushapalle <anushapalle11@gmail.com>
2023-10-08 22:34:21 +0530

    Add: Decision structures: if else, switch

--- Trevor-Sawler-Udemy-Modern-WebApps-Go/main.go
+++ Trevor-Sawler-Udemy-Modern-WebApps-Go/main.go
@@ -99,6 +99,61 @@ func main() {
 	names := []string{"one", "seven", "fish", "cat"}
 	log.Println(names)
 
+	// the if statement
+	var isTrue bool
+
+	isTrue = false
+	
+	if isTrue == true {
+		log.Println("isTrue is", isTrue)
+	} else {
+		log.Println("isTrue is", isTrue)
+	}
+	
+	cat := "cat2"
+	
+	if cat == "cat" {
+		log.Println("Cat is cat")
+	} else {
+		log.Println("Cat is not cat")
+	}
+	
+	myNum := 100
+	isTrue = false
+	
+	if myNum > 99 && !isTrue {
+		log.Println("myNum is greater than 99 and isTrue is set to true")
+	} else if myNum < 100 && isTrue {
+		log.Println("myNum is less than 100 and isTrue is set to true")
+	} else if myNum == 101 || isTrue {
+		log.Println("myNum is == 101 or isTrue is set to true")
+	} else if myNum > 1000 && isTrue {
+		log.Println("myNum is greater than 1000 and isTrue is set to true")
+	} else {
+		log.Println("myNum is matching to none")
+	}
+	
+	// the switch statement
+	
+	myVar := "cat"
+	
+	switch myVar {
+	case "cat":
+		log.Println("myVar is set to cat")
+	
+	case "dog":
+		log.Println("myVar is set to dog")
+	
+	case "horse":
+		log.Println("myVar is set to horse")
+	
+	case "fish":
+		log.Println("myVar is set to fish")
+	
+	default:
+		log.Println("myVar is something else")
+	}
+
 }
 
 // custom functions 
