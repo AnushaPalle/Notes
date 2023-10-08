98ccec1168f1e07899e1ac238a2aa09665a8487a
anushapalle <anushapalle11@gmail.com>
2023-10-08 23:11:07 +0530

    Add: Interfaces

--- Trevor-Sawler-Udemy-Modern-WebApps-Go/main.go
+++ Trevor-Sawler-Udemy-Modern-WebApps-Go/main.go
@@ -25,6 +25,25 @@ func (u *User) getFirstName() string {
 	return u.FirstName
 }
 
+// Animal defines the interface for type Animal
+type Animal interface {
+	Says() string
+	NumberOfLegs() int
+}
+
+// Dog defines the dog type
+type Dog struct {
+	Name  string
+	Breed string
+}
+
+// Gorilla defines the Gorilla type
+type Gorilla struct {
+	Name          string
+	Color         string
+	NumberOfTeeth int
+}
+
 func main() {
 	fmt.Println("Hello Ganesh !!")
 
@@ -171,6 +190,29 @@ func main() {
 		log.Println(l.FirstName, l.LastName, l.Email, l.Age)
 	}
 
+	dog := Dog{
+		Name:  "Samson",
+		Breed: "German Shepherd",
+	}
+
+	// We can pass dog to PrintInfo(), since the Dog type implements the Animal interface by having all of the
+	// necessary functions. The parameter is passed as a pointer since the functions for the type have pointer
+	// receivers (which is the norm. See https://tour.golang.org/methods/4 and
+	// https://tour.golang.org/methods/8 for more details).
+	PrintInfo(&dog)
+
+	gorilla := Gorilla{
+		Name:          "Jock",
+		Color:         "grey",
+		NumberOfTeeth: 38,
+	}
+
+	// We can also pass gorilla to PrintInfo(), since the Gorilla type implements the Animal interface by having all of the
+	// necessary functions. The parameter is passed as a pointer since the functions for the type have pointer
+	// receivers (which is the norm. See https://tour.golang.org/methods/4 and
+	// https://tour.golang.org/methods/8 for more details).
+	PrintInfo(&gorilla)
+
 }
 
 // custom functions 
@@ -182,4 +224,30 @@ func changeUsingPointer(s *string) {
 	log.Println("s is set to", s)
 	newValue := "Red"
 	*s = newValue
+}
+
+func PrintInfo(a Animal) {
+	fmt.Println("This animal says", a.Says(), "and has", a.NumberOfLegs(), "legs")
+}
+
+// Says has a receiver of type *Dog, so it satisfies part of the interface requirements for Animal
+// for the Dog type
+func (d *Dog) Says() string {
+	return "Woof"
+}
+
+// NumberOfLegs satisfies the rest of the Animal interface requirements for the Dog type
+func (d *Dog) NumberOfLegs() int {
+	return 4
+}
+
+// Says has a receiver of type *Gorilla, so it satisfies part of the interface requirements for Animal
+// for the Gorilla type
+func (d *Gorilla) Says() string {
+	return "Ugh"
+}
+
+// NumberOfLegs satisfies the rest of the Animal interface requirements for the Gorilla type
+func (d *Gorilla) NumberOfLegs() int {
+	return 2
 }
